96980ac73de6dbaa8ee6ff5c14af39a4fefa0e35
johnom-commits <johnom2012@yandex.ru>
2020-12-27 19:52:56 +0300

    IndexControl v.3

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,17 @@ import ru.job4j.accident.service.AccidentService;
 
 @Controller
 public class IndexControl {
+
+    private AccidentService service;
+
+    @Autowired
+    public IndexControl(AccidentService service) {
+        this.service = service;
+    }
+
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("accidents", new AccidentService().accidents());
+        model.addAttribute("accidents", service.accidents());
         return "index";
     }
 }
--- src/main/java/ru/job4j/accident/service/AccidentService.java
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -1,5 +1,6 @@
 package ru.job4j.accident.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.repository.AccidentMem;
@@ -9,7 +10,14 @@ import java.util.Collection;
 @Service
 public class AccidentService {
 
+    private AccidentMem store;
+
+    @Autowired
+    public AccidentService(AccidentMem store) {
+        this.store = store;
+    }
+
     public Collection<Accident> accidents() {
-        return new AccidentMem().getAccidents();
+        return store.getAccidents();
     }
 }
