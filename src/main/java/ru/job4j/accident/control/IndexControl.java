2cf495cbc2b6fa5ea21e80b5d32d7fb8baa7f7dc
johnom-commits <johnom2012@yandex.ru>
2021-01-07 14:46:12 +0300

    Auth through jdbc

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -1,6 +1,7 @@
 package ru.job4j.accident.control;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class IndexControl {
 
     @GetMapping("/")
     public String index(final Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", service.accidents());
         return "index";
     }
